bc27543d55b2d47e1e39a5c0743142ed2bed9386
user <user@user-PC>
2016-10-04 23:01:17 +0800

    第三版

--- src/fcu/iecs/oop/Hello.java
+++ src/fcu/iecs/oop/Hello.java
@@ -4,6 +4,7 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 		String lyrics = "Let it go! Let it go! Cannot hold it back anymore"; 
 		lyrics = lyrics.replaceAll("it", "\"her\""); 
 		lyrics = lyrics.replaceAll("Cannot", "Can't"); 
